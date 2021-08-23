83c44b9c983f0cb9e5b48c324c6ded5abfe248c8
Matěj Jehlička <jehlicka@mjehlicka.eu>
2021-08-23 17:02:36 +0200

    Update build-and-upload-frontend.sh

--- build-and-upload-frontend.sh
+++ build-and-upload-frontend.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-cd /home/dizider/Documents/edu/BP/impl/a/device-front && \
+
+# FIRMWARE_PATH -> path to repositiory with tracker firmware
+# FRONT_PATH -> path to repositiory with tracker UI
+
+while getopts u:f: flag
+do
+    case "${flag}" in
+        f) FIRMWARE_PATH=${OPTARG};;
+        u) FRONT_PATH=${OPTARG};;
+    esac
+done
+
+cd $FRONT_PATH && \
 yarn build && \
 cd dist && \
 
@@ -10,8 +22,16 @@ cd .. && \
 gzip -9 css/* && \
 gzip -9 js/* && \
 
-cd /home/dizider/Documents/edu/BP/impl/a/device-front && \
-cp -r dist/* /home/dizider/CLionProjects/untitled/data/w && \
-du -b /home/dizider/CLionProjects/untitled/data/* && \
+cd $FRONT_PATH && \
+
+FIRMWARE_PATH_DATA=$FIRMWARE_PATH/data/w
+
+if ! [[ -d "$FIRMWARE_PATH_DATA" ]]; then
+`mkdir -p $FIRMWARE_PATH_DATA`;
+echo "$FIRMWARE_PATH_DATA directory is created"
+fi
+
+cp -r dist/* $FIRMWARE_PATH/data/w && \
+du -b $FIRMWARE_PATH/data/* && \
 read -p "Press any key to upload (or kill me)..." && \
 pio run --target uploadfs
